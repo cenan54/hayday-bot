3608889919e18eae06872af46ee75139a5de0c87
Mustafa Cenan <54mustafacenan54@gmail.com>
2025-04-12 23:17:28 +0300

    added starter counter and basic mouse clicker in main

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import pyautogui
 import cv2 as cv
 import time
+import sys
 
 def main():
+    #Initialized pyautogui 
+    pyautogui.FAILSAFE = True
+
+    #Countdown timer before starting the app
+    print("Uygulama 10 saniye sonra basliyor",end="")
+    for i in range(0,10):
+        print(".",end="")
+        sys.stdout.flush()#forces to appear dots in real time
+        time.sleep(1)
+    print("Basla")
+
+    #Clicks center of screen print("App starting",end="")
+    for i in range(0,10):
+        print(".",end="")
+    screenWidht,screenHeight = pyautogui.size()
+    centerWidth = screenWidht // 2
+    centerHeigth = screenHeight // 2
+
+    pyautogui.click(centerWidth,centerHeigth)
 
 
 if __name__=="__main__":
